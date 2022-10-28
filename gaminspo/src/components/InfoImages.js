ac103f2adcb71ef0164d10a7ce5fdb78d8943506
Pablo <pablohern21gc@gmail.com>
2022-10-28 20:43:55 +0100

    simplified cols and searchbar works

---
+++ gaminspo/src/components/InfoImages.js
@@ -0,0 +1,12 @@
+export const infoRoutes = [
+  {
+    id: 1,
+    name: "epic capybara",
+    img: "img/capylogo.png"
+  },
+  {
+    id: 2,
+    name: "koala",
+    img: "img/koala.jpg"
+  }
+];
--- gaminspo/src/pages/MainPage.js
+++ gaminspo/src/pages/MainPage.js
@@ -7,12 +7,36 @@ import '../styles/MainPage.css';
 import { Card, Space } from 'antd';
 import Footer from '../components/Footer';
 import Menu from '../components/MainMenu';
+import React, { useState, useEffect } from 'react';
 const { Title } = Typography;
 const { Link } = Typography;
 export default function MainPage() {
   const { Search } = Input;
   const { Meta } = Card;
-  const onSearch = (value) => console.log(value);
+  const onSearch = (value) => setValue(value);
+  const [data, setData] = useState([]);
+  const [value, setValue] = useState([]);
+  const getData = () => {
+    fetch('/data.json'
+      , {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    )
+      .then(function (response) {
+        console.log(response)
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        setData(myJson)
+      });
+  }
+  useEffect(() => {
+    getData()
+  }, [])
   return (
     <Space direction="vertical" size="large" style={{ display: 'flex' }}>
       <Header></Header>
@@ -25,58 +49,29 @@ export default function MainPage() {
         <Menu></Menu>
         <div className='gallery'>
           <Row justify="space-around">
-            <div className="card_div">
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="img/capylogo.png" />}>
-                <Meta title="Epic Carpincho" description="Carpinchos are the best" />
-              </Card>
-            </div>
-            <div className="card_div">
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="img/capylogo.png" />}>
-                <Meta title="Epic Carpincho" description="Carpinchos are the best" />
-              </Card>
-            </div>
-          </Row>
-          <Row justify="space-around">
-            <div className="card_div">
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="img/capylogo.png" />}>
-                <Meta title="Epic Carpincho" description="Carpinchos are the best" />
-              </Card>
-            </div>
-            <div className="card_div">
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="img/capylogo.png" />}>
-                <Meta title="Epic Carpincho" description="Carpinchos are the best" />
-              </Card>
-            </div>
-          </Row>
-          <Row justify="space-around">
-            <div className="card_div">
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="img/capylogo.png" />}>
-                <Meta title="Epic Carpincho" description="Carpinchos are the best" />
-              </Card>
-            </div>
-            <div className="card_div">
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="example" src="img/capylogo.png" />}>
-                <Meta title="Epic Carpincho" description="Carpinchos are the best" />
-              </Card>
-            </div>
+            {(data && data.length > 0 && (value == "" || value == null)) ? data.map(e => (
+              <Col span={8}>
+                <div className="card_div">
+                  <Card
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="example" src={e.img} />}>
+                    <Meta title={e.name} description={e.info} />
+                  </Card>
+                </div>
+              </Col>
+            )) : data.filter(e => e.name == value).map(e => (
+              <Col span={8}>
+                <div className="card_div">
+                  <Card
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="example" src={e.img} />}>
+                    <Meta title={e.name} description={e.info} />
+                  </Card>
+                </div>
+              </Col>
+            ))}
           </Row>
         </div>
       </div>
